317def9a85c60f839ce8605e6cb15ab7387dc50f
AkashGupta41-code <akashroyal3443@gmail.com>
2025-03-30 00:15:13 +0530

    Initial commit with index.html and other files

---
+++ dsa.js
@@ -0,0 +1,25 @@
+// align content 
+//  z index  = overlaps
+// box model 
+// let 
+// var
+// const
+// tempraled red zone function block scope
+// spread concstion 
+// destructure
+// rest operater dont know haw many 
+
+/* node js*/
+// javascript is a scripting launguage 
+// but node js is a runtime invirment for javascript 
+//  single threaded based on event driven architecht
+// asyncronos funtion block the code 
+// module files are the data or we can say code of the nodejs libararies 
+// npm node package manager  for install pakcage
+// folder, npm init,express
+// htttp module
+// express
+// middlewares 
+
+
+
---
+++ int.js
@@ -0,0 +1,11 @@
+// scope we have three scope
+// shadowing overight in block only outside the block value are still
+// declaration 
+// declariton let var  but not  const
+// re-initialization  var let but not const
+// hoisting on top of code in only var and let temparrled red zone
+
+
+
+
+
